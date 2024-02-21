d4bd20b88fc190739b7c8efe74a9bd60d39cd746
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-02-20 22:38:57 -0300

    Finalização do projeto Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 
 import "./App.css";
+import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -26,6 +28,11 @@ function App() {
       isCompleted: false,
     },
   ]);
+
+  const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -53,16 +60,39 @@ function App() {
     newTodos.map((todo) =>
       todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
     );
-    setTodos(newTodos)
+    setTodos(newTodos);
   };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
-        ))}
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const Search = () => {
+const Search = ({ search, setSearch }) => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <h2>Pesquisar:</h2>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Digite para pesquisar..."
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
