7242b468338e12597ed2c5e56cea3bc066e5c5ae
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-02-20 21:02:20 -0300

    Adição do formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
+
+import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -21,7 +25,31 @@ function App() {
       isCompleted: false,
     },
   ]);
-  return <div>React</div>;
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
 export default App;
